4a5a9ac00bcf76a54e4bc78015a11d1debf87f1e
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2022-11-21 11:58:33 -0700

    Completed Declaring and Initializing

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare & Initilize 
+            string name = "Brandon";
+            int age = 29;
+            char middleInitial = 'P';
+            bool isOver18 = true;
+            double curretTemp = 77.9;
+            decimal currentPrice = 29.99m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Over 18?: {isOver18}");
+            Console.WriteLine($"Curret Temperature: {curretTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
         }
     }
 }
